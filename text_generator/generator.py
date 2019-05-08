bf9c3bc205df43404c79d960c9b936928bcf6376
Aseem Patni <aseem@aseempatni.com>
2019-05-07 20:34:50 -0700

    Added generator

---
+++ text_generator/generator.py
@@ -0,0 +1,19 @@
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+
+# tokenizer = Tokenizer()
+
+
+def generate_text(seed_text, next_words, max_sequence_len, model, tokenizer):
+    for _ in range(next_words):
+        token_list = tokenizer.texts_to_sequences([seed_text])[0]
+        token_list = pad_sequences([token_list], maxlen=max_sequence_len - 1, padding='pre')
+        predicted = model.predict_classes(token_list, verbose=0)
+
+        output_word = ""
+        for word, index in tokenizer.word_index.items():
+            if index == predicted:
+                output_word = word
+                break
+        seed_text += " " + output_word
+    return seed_text
